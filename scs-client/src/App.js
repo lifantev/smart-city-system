b4a56b9dc657fba89871b51b416758486ce6de91
Alexander Zubkov <zubkov.as@phystech.edu>
2021-08-25 17:28:50 +0300

    [SCS-30] ENV variables for React Client
    Environment variables are available from public/properties.json

--- scs-client/src/App.js
+++ scs-client/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 
 function App() {
-  return (
-    <div className="App">
-      Hello world 2!
-    </div>
-  );
+
+    const [isLoaded, setLoaded] = useState(false);
+
+    useEffect(
+        () => {
+            axios.get("/properties.json").then(
+                (data) => {
+                    localStorage.setItem("scs-properties", JSON.stringify(data.data))
+                    setLoaded(true)
+                }
+            )
+        },
+        [])
+
+    return isLoaded ?
+        <div className="App">
+            Hello world 2!
+        </div> :
+        <div>Loading</div>;
 }
 
 export default App;
