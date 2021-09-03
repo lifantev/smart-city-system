4eeec1ce1da38cb45ccf38944d1abcfdc48de6cd
lifantev <Lifantev.DA@phystech.edu>
2021-09-03 21:31:37 +0300

    [SCS-39] Create docker-compose for 3 clusters

--- scs-backend-haproxy/config.py
+++ scs-backend-haproxy/config.py
@@ -1,20 +1,19 @@
 import os
 
-#backends_line = os.environ.get('SCS_BACKENDS')
-#port = os.environ.get('SCS_BACKEND_PORT')
-backends_line = 'cdev-backend-1 cdev-backend-2'
-port = 8080
+backends_line = os.environ.get('SCS_BACKENDS')
+port = os.environ.get('SCS_BACKEND_PORT')
 
 backends = backends_line.split()
-config_format = 'server %s %s:%d check\n\t'
+config_format = '\n            server %s %s:%s check'
 
 config_block = ''
-for server in backends :
+for server in backends:
     config_block += config_format % (server, server, port)
+config_block += '\r\n'
 
 path = '/usr/local/etc/haproxy/haproxy.cfg'
 
-with open(path, 'r') as file :
+with open(path, 'r') as file:
   filedata = file.read()
 
 filedata = filedata.replace('{$SCS_BACKENDS}', config_block)
